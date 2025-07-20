f78218fd4d970bc33ff10d3a90e44eb872050f8f
Mokesh-07 <63835473+Mokesh-07@users.noreply.github.com>
2025-07-20 17:40:49 +0530

    Add files via upload

---
+++ 2025/Project - 4 Synthesis to Tape-Out: RTL to GDSII Flow for 4-bit Up Counter/physical verification/lvs/saveNetlistCtl.tcl
@@ -0,0 +1,5 @@
+set runDir {/home/M24_2/cadence_edt/mokesh_new/physical_design_90/trial_new/lvs_1}
+set phys true
+set excludeLeafCell true
+set includePowerGround true
+set additionalSaveNetlistParameters {}
