c7fcbe26e0da6280a069633a8af2d14c6810e6d9
Mokesh-07 <63835473+Mokesh-07@users.noreply.github.com>
2026-03-30 06:35:53 +0530

    Add files via upload

---
+++ 2026/Project - 2 Design for Testability of Round-Robin Arbiter with Automatic Test Pattern Generation/dft_insertion_for_scan_chain/fv/rr_arbiter/read_libs.tcl
@@ -0,0 +1,4 @@
+add_search_path /home/M24_2/cadence_edt/HMK_VDA/mokesh_project_test_arbiter/dft_insertion_for_scan_chain/dft_input -library -both
+read_library -liberty -both \
+    /home/M24_2/cadence_edt/HMK_VDA/mokesh_project_test_arbiter/dft_insertion_for_scan_chain/dft_input/slow.lib
+
